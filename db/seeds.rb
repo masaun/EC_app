e72ab62d6432fa03c98fee5798aeff342fb53b7b
masaun <unomasanori@unomasanori-no-MacBook-Air.local>
2016-10-16 20:55:23 +0900

    add_validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+
+Product.create(title: 'Test code 2016.10.16',
+  description:
+    %{<p>
+      test text code.
+      </p>},
+  image_url: 'test.jpg',
+  price: 50.55)
